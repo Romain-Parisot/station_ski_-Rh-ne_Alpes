a9eaa65ff81b74e2665bfe3711a00f45e7165ed7
DragiboO <dragibo.scotch@gmail.com>
2023-03-20 17:06:32 +0100

    dashboard station

--- src/Controller/Admin/StationDashboardController.php
+++ src/Controller/Admin/StationDashboardController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Domaine;
+use App\Entity\Piste;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -10,9 +10,9 @@ use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DashboardController extends AbstractDashboardController
+class StationDashboardController extends AbstractDashboardController
 {
-    #[Route('/admin', name: 'admin')]
+    #[Route('/station', name: 'station')]
     public function index(): Response
     {
         //return parent::index();
@@ -20,7 +20,7 @@ class DashboardController extends AbstractDashboardController
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        return $this->redirect($adminUrlGenerator->setController(DomaineCrudController::class)->generateUrl());
+        return $this->redirect($adminUrlGenerator->setController(StationCrudController::class)->generateUrl());
 
         // Option 2. You can make your dashboard redirect to different pages depending on the user
         //
@@ -37,12 +37,12 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Ski Rhones Alpes');
+            ->setTitle('Station jsp');
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('The Label', 'fas fa-list', Domaine::class);
+        yield MenuItem::linkToCrud('The Label', 'fas fa-list', Piste::class);
     }
 }
