b33b5d7c974265f87aa74efd20e784698a6d4342
deez-blip <gaspard.castagnet92@gmail.com>
2023-03-26 15:30:25 +0200

    lien domaine -> stations

--- src/Controller/StationController.php
+++ src/Controller/StationController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Station;
+use App\Repository\DomaineRepository;
 use App\Repository\PisteRepository;
 use App\Repository\StationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,16 +13,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class StationController extends AbstractController
 {
     #[Route('/station/{id}', name: 'app_station')]
-    public function show($id, StationRepository $stationRepository, PisteRepository $pisteRepository): Response
+    public function show($id, StationRepository $stationRepository, PisteRepository $pisteRepository, DomaineRepository $domaineRepository): Response
     {
-        $station = $stationRepository->find($id);
+
+        $domaine = $domaineRepository->find($id);
+
+        if (!$domaine) {
+            throw $this->createNotFoundException('Domaine not found');
+        }
+
+        $station = $stationRepository->findBy(['domaine' => $domaine]);
 
         if (!$station) {
             throw $this->createNotFoundException('Station not found');
         }
 
         return $this->render('station/index.html.twig', [
-            'station' => $station,
+            'stations' => $station,
             'piste' => $pisteRepository->findAll()
         ]);
     }
