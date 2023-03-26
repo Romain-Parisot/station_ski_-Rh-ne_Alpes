d2610cfb50e1fb72eb29a2c177866366d2cae718
Ali ABBAS <ali.ibra.abbas@gmail.com>
2023-03-26 12:31:38 +0200

    ADD domain.css + Style de la page domaine

--- src/Controller/DomaineController.php
+++ src/Controller/DomaineController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Repository\StationRepository;
+
 use App\Repository\DomaineRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DomaineController extends AbstractController
 {
     #[Route('/domaine', name: 'app_domaine')]
-    public function index( DomaineRepository $domaineRepository): Response
+    public function index(DomaineRepository $domaineRepository): Response
     {
 
         return $this->render('domaine/index.html.twig', [
